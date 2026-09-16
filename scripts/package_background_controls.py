a9cdb42be64a38a0ab42cf09b8d348106037269d
mahfouzhassine8-ops <mahfouzhassine8@gmail.com>
2026-09-16 02:47:06 -0400

    Add background controls RC2 packager wrapper

---
+++ scripts/package_background_controls.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Package Background Controls RC2 using the proven RC1 Android-only packager."""
+import package_background_resume as package
+
+package.RELEASE = '1.0.9-Cobra-Background-Controls-RC2'
+package.VERSION_CODE = 2103137
+
+if __name__ == '__main__':
+    package.main()
